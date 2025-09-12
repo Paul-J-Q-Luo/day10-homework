bf658e50c5ac590cd4b59f480a893efac3e93076
Paul J Q Luo <paul.j.q.luo@oocl.com>
2025-09-12 15:06:52 +0800

    feat: Implement todo list functionality with context and reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import {createContext, useContext, useReducer} from "react";
+
+export const TodoContext = createContext();
+
+function TodoItem(props) {
+    const [state, dispatch] = useContext(TodoContext);
+
+    function makeDone() {
+        dispatch({
+            type: "TOGGLE_TODO",
+            payload: {id: props.todo.id}
+        })
+    }
+
+    return <div className={"todo-item"}>
+        <span className={props.todo.done ? "todo-done" : ""}
+              onClick={makeDone}>
+            {props.todo.text}
+        </span>
+    </div>;
+}
+
+function TodoGroup() {
+    const [state, dispatch] = useContext(TodoContext);
+
+    return <div>
+        {
+            state.map((item, index) => {
+                return <TodoItem key={index} todo={item} index={index}/>
+            })
+        }
+    </div>
+}
+
+export const initData = [
+    {id: 1, text: "the first todo", done: false},
+    {id: 2, text: "the second todo", done: true}
+];
+
+export function todoReducer(state, action) {
+    switch (action.type) {
+        case "TOGGLE_TODO":
+            const newState = [...state];
+            const id = action.payload.id;
+            return newState.map((value) => {
+                if (value.id === id) {
+                    return {
+                        id,
+                        text: value.text,
+                        done: !value.done
+                    };
+                }
+                return value;
+            })
+        default:
+            return state;
+    }
+    return state;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = useReducer(todoReducer, initData);
+    return (
+        <div className="App">
+            <TodoContext.Provider value={[state, dispatch]}>
+                <TodoGroup/>
+            </TodoContext.Provider>
+        </div>
+    );
 }
 
 export default App;
