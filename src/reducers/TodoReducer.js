ff99e5336fd81844c07e1c3d3f4e66ae33475d1f
Paul J Q Luo <paul.j.q.luo@oocl.com>
2025-09-13 14:03:51 +0800

    feat: Implement ADD_TODO action in todoReducer for adding new todos

--- src/reducers/TodoReducer.js
+++ src/reducers/TodoReducer.js
@@ -1,5 +1,7 @@
 export function todoReducer(state, action) {
     switch (action.type) {
+        case "ADD_TODO":
+            return [...state, action.payload];
         case "TOGGLE_TODO":
             return state.map(todo =>
                 todo.id === action.payload.id ? {...todo, done: !todo.done} : todo
