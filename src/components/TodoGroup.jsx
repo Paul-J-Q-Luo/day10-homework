9b200d1622803c38ada472c37185a4076fc54bf3
Paul J Q Luo <paul.j.q.luo@oocl.com>
2025-09-13 15:01:28 +0800

    feat: Add todo completion statistics display in TodoGroup component

--- src/components/TodoGroup.jsx
+++ src/components/TodoGroup.jsx
@@ -6,8 +6,15 @@ import {TodoContext} from "../contexts/TodoContext";
 export function TodoGroup() {
     const [state, dispatch] = useContext(TodoContext);
 
+    const totalTodos = state.length;
+    const completedTodos = state.filter(item => item.done).length;
+
     return <div>
         <div className="todo-list-wrapper">
+            {totalTodos > 0 && (
+                <p className="todo-stats">{completedTodos} of {totalTodos} tasks completed</p>
+            )}
+
             {state.length > 0 ? (
                 state.map((item) => {
                     return <TodoItem key={item.id} todo={item} />;
