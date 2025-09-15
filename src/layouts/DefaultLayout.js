f7cfb80c6cd090bd6002218f9ca2ce27b25836f5
Paul J Q Luo <paul.j.q.luo@oocl.com>
2025-09-15 12:51:05 +0800

    refactor: Refactor DefaultLayout with improved styling and structure for navigation

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,18 +1,20 @@
 import {NavLink, Outlet} from "react-router";
 
 export function DefaultLayout() {
-    return <div>
-        <header>
-            <nav>
-                <ul>
-                    <li><NavLink to={"/"}>Home</NavLink></li>
-                    <li><NavLink to={"/done"}>Done List</NavLink></li>
-                    <li><NavLink to={"/about"}>About us</NavLink></li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <Outlet/>
-        </main>
-    </div>
+    return (
+        <div className="layout-container">
+            <header className="main-header">
+                <nav className="main-nav">
+                    <ul>
+                        <li><NavLink to={"/"}>Home</NavLink></li>
+                        <li><NavLink to={"/done"}>Done List</NavLink></li>
+                        <li><NavLink to={"/about"}>About</NavLink></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <Outlet/>
+            </main>
+        </div>
+    );
 }
