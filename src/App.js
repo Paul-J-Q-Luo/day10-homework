3e07865d061c66ac2f9112f138ca6718b5d6f11e
Paul J Q Luo <paul.j.q.luo@oocl.com>
2025-09-15 11:09:25 +0800

    feat: Refactor routing by separating layout and routes into dedicated files

--- src/App.js
+++ src/App.js
@@ -2,38 +2,8 @@ import './App.css';
 import {useReducer} from "react";
 import {todoReducer} from "./reducers/TodoReducer";
 import {TodoContext} from "./contexts/TodoContext";
-import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
-import {ErrorPage} from "./pages/ErrorPage";
-import {HomePage} from "./pages/HomePage";
-
-function DefaultLayout() {
-    return <div>
-        <header>
-            <nav>
-                <ul>
-                    <li><NavLink to={"/"}>Home</NavLink></li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <Outlet/>
-        </main>
-    </div>
-}
-
-const routes = createBrowserRouter([
-    {
-        path: "/",
-        element: <DefaultLayout />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: "/",
-                element: <HomePage />
-            }
-        ]
-    }
-])
+import {RouterProvider} from "react-router";
+import {routes} from "./routes/Routes";
 
 export const initData = [
     {id: 1, text: "the first todo", done: false},
---
+++ src/pages/TodoDetailPage.js
@@ -0,0 +1,18 @@
+import {useParams} from "react-router";
+import {useContext} from "react";
+import {TodoContext} from "../contexts/TodoContext";
+import {TodoItem} from "../components/TodoItem";
+
+export function TodoDetailPage() {
+    const {id} = useParams()
+    const [state] = useContext(TodoContext)
+    const todo = state.filter((todo) => todo.id === parseInt(id))
+
+    if (todo.length === 0) {
+        return <div>Todo not found</div>
+    }
+
+    return <div>
+        <TodoItem todo={todo[0]} index={id}></TodoItem>
+    </div>;
+}
